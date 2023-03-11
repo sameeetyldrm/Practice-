65ec616dd6bb06f3ce1a62c2ccce00a19c30a9ac
Samet YILDIRIM <sametyildirim2705@gmail.com>
2023-03-11 16:15:04 +0300

    6

---
+++ odev6.sql
@@ -0,0 +1,23 @@
+1
+
+SELECT ROUND(AVG(rental_rate),3) FROM film;
+
+--2
+
+SELECT COUNT(title) FROM film
+WHERE title Like 'C%';
+
+--3
+
+SELECT MAX(length) FROM film
+WHERE rental_rate=0.99;
+
+--4
+
+SELECT COUNT(DISTINCT(replacement_cost)) FROM film
+WHERE length>150;
+
+
+
+
+

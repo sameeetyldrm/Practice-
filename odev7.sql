3a45450f1e50a344d949b895133d5a23942d848f
Samet YILDIRIM <sametyildirim2705@gmail.com>
2023-03-11 17:25:28 +0300

    7

---
+++ odev7.sql
@@ -0,0 +1,23 @@
+--1
+
+SELECT rating,COUNT(*) FROM film
+GROUP BY rating;
+
+--2
+
+SELECT replacement_cost,COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING  COUNT(*)> 50
+ORDER BY replacement_cost;
+
+--3
+
+SELECT store_id,COUNT(*) FROM customer
+GROUP BY store_id;
+
+--4
+
+SELECT country_id,COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(country_id) DESC
+LIMIT 1;
